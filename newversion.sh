ad64e0fe92515f33ffa420f492399f3df0853c69
Wagner Sales <salesawagner@gmail.com>
2017-04-17 16:31:19 -0300

    teste

---
+++ newversion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo Input version number:
+read versionNumber
+
+echo "### Creating version number: $versionNumber"
+
+# Create branch
+git checkout -b development
+#git reset --hard
+#git clean -fd
+git checkout -b feature/"$versionNumber"
+git branch
+
+# Modify versions
+agvtool bump -all
+agvtool new-marketing-version $versionNumber
+
+# Git push
+git add --all
+git commit -m \"new version $versionNumber\"
+git status
